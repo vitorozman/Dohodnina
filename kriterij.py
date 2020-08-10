8a192cef24a3afa7539a1d60eed760cb8be1cb79
Vito Rozman <rozman.vito@gmali.com>
2020-08-10 19:26:03 +0200

    zacetni kriterij in nekaj prvih funkcij

---
+++ kriterij.py
@@ -0,0 +1,20 @@
+
+#znacke olajsav
+
+SPLOSNA = {'S': 0} #splosna olajsava
+
+OSEBNA = {'O' : 1471.57} #osebna olajsava
+
+POSEBNA_OSEBNA = {'P' : 3500} #posebna osebna olajsava na letni zasluzek
+
+POSEBNA_OTROSKA = {'Po' : 0}  #posebna olajsava za vzdrzevanega otroka
+
+POSEBNA_OTROSKA_INV = {'PoI' : 735.83} #posenbna olajsava za vzdrzevanega otroka, ki potrebuje posebno nego
+
+POSEBNA_CLAN = {'Pc' : 203.08} # posebna olajsava za vdrzevanega druziskega clana
+
+DODATNO_POKOJNINSKO = {'Z' : 0} #olajsava za prostovoljno dodatno pokojninsko zavarovanje
+
+
+
+
--- model.py
+++ model.py
@@ -1,7 +1,34 @@
 
-
 class Dohodnina_1:
-    pass
+    
+    def __init__(self, placa, sez_znacka):#sez_znacka vsebuje znacke ki predstavljajo dolocene olajsave, placa je bruto mesecna placa
+        self.placa = placa 
+        self.sez_znacka = sez_znacka
+    
+    def prispevki(self):
+        if student in self.sez_znacka and vzdrzevan in self.sez_znacka:
+            return self.placa * 0.155
+        elif upokojenec in self.sez_znacka:
+            return 0
+        else:
+            return self.placa * 0.221
+
+
+    def racunalo(self):
+        #placa = prispevki(self.placa)
+        pass
 
 class Dohodnina_2(Dohodnina_1):
-    pass
+
+    def __init__(self, placa1, placa2, sez_znacka1, sez_znacka2):
+        self.placa1 = placa1
+        self.placa2 = placa2
+        self.sez_znacka1 = sez_znacka1
+        self.sez_znacka2 = sez_znacka2
+
+
+def splona(placa):
+    if placa <= 1109.74:
+        return 291.67 + (1558.37 - 1404.27 * placa)
+    else:
+        return 291.67
