aef54c1c04875d752dcaadbf2878b6909c86e862
Vito Rozman <rozman.vito@gmali.com>
2020-08-11 22:45:26 +0200

    ustvaril racunalo, prispevki, olajsave #potrebna izboljsava

--- kriterij.py
+++ kriterij.py
@@ -8,7 +8,13 @@ OSEBNA = {'O' : 1471.57} #osebna olajsava
 POSEBNA_OSEBNA = {'P' : 3500} #posebna osebna olajsava na letni zasluzek
 
 POSEBNA_OTROSKA = {'Po' : 0}  #posebna olajsava za vzdrzevanega otroka
-
+na_otroka = {
+        1:2436.92,
+        2:2649.24,
+        3:4418.54,
+        4:6187.85,
+        5:7957.14
+        }
 POSEBNA_OTROSKA_INV = {'PoI' : 735.83} #posenbna olajsava za vzdrzevanega otroka, ki potrebuje posebno nego
 
 POSEBNA_CLAN = {'Pc' : 203.08} # posebna olajsava za vdrzevanega druziskega clana
--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+from kriterij import *
+
 
 class Dohodnina_1:
     
@@ -6,17 +8,64 @@ class Dohodnina_1:
         self.sez_znacka = sez_znacka
     
     def prispevki(self):
-        if student in self.sez_znacka and vzdrzevan in self.sez_znacka:
+        if 'P' in self.sez_znacka:
             return self.placa * 0.155
-        elif upokojenec in self.sez_znacka:
+        elif 'Up' in self.sez_znacka:
             return 0
         else:
             return self.placa * 0.221
 
+    def olajsave(self, st_otrok, st_inv_otrok, st_vzd_clan, dodatno_pok):
+        olajsava = 0
+        if 'O' in self.sez_znacka:
+            olajsava = 17658.84
+        if 'P' in self.sez_znacka:
+            olajsava += 3500
+        if 'Po' in self.sez_znacka:
+            na_dodatnega = na_otroka[5]
+            for otrok in range(1, st_otrok + 1):  # za vsakega otroka pogleda koliko je olajsava in doda
+                if otrok >= 6:
+                    na_dodatnega += 1769.3
+                    olajsava += na_dodatnega
+                else:
+                    olajsava += na_otroka[otrok]
+        if 'PoI' in self.sez_znacka:
+            olajsava += st_inv_otrok * 8830
+        if 'Pc' in self.sez_znacka:
+            olajsava += st_vzd_clan * 2436.62
+        if 'Z' in self.sez_znacka:
+            olajsava += min(2819.09, self.placa * 0.05844, dodatno_pok)
+        return olajsava
+    
 
+############## letno ##############
+def racunalo(self):
+        osnova = self.placa - self.prispevki() - olajsave()
+        if osnova <= 8500:
+            return self.placa * 0.16
+        elif 8500 < osnova <= 25000:
+            return 1360 + (osnova - 8500) * 0.26
+        elif 25000 < osnova <= 50000:
+            return 5650 + (osnova - 25000) * 0.33
+        elif 25000 < osnova <= 72000:
+            return 13900 + (osnova - 4166.67) * 0.39
+        else:
+            return 22480  + (osnova - 72000) * 0.5
+
+
+############# mesecno #############
     def racunalo(self):
-        #placa = prispevki(self.placa)
-        pass
+        osnova = self.placa - self.prispevki() - olajsave()
+        if osnova <= 708.33:
+            return self.placa * 0.16
+        elif 708.33 < osnova <= 2083.33:
+            return 113.33 + (osnova - 708.33) * 0.26
+        elif 1083.33 < osnova <= 4166.67:
+            return 470.83 + (osnova - 2083.33) * 0.33
+        elif 4166.67 < osnova <= 6000:
+            return 1158.33 + (osnova - 4166.67) * 0.39
+        else:
+            return 1873.33  + (osnova - 6000) * 0.5
 
 class Dohodnina_2(Dohodnina_1):
 
