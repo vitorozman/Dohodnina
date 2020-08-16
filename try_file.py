e71902dff359de68f0a9846e23be4555d49b294a
Vito Rozman <rozman.vito@gmali.com>
2020-08-16 19:45:56 +0200

    file za testiranje

---
+++ try_file.py
@@ -0,0 +1,20 @@
+from new_model import * 
+
+sez = na_otroka
+m = moznosti()
+p1 = 5000
+sl = {
+    'splosna' : True,
+    'invalidska' : True,
+    'dodatno_pok' : 200
+}
+st = 3
+pr1 = p1 * 0.22
+
+p2 = 4000
+pr2 = p2 * 0.22
+
+d1 = Dohodnina(p1, pr1, sl, st)
+d2 = Dohodnina(p2, pr2, sl, st)
+
+print(optimalec(d1, d2))
