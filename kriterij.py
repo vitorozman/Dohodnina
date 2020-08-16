8dadacc59e129c8aa3c45c50dfd81b0679824cd4
Vito Rozman <rozman.vito@gmali.com>
2020-08-16 19:39:01 +0200

    izbrisal nekaj neuporabnih stvari

--- kriterij.py
+++ kriterij.py
@@ -3,9 +3,10 @@
 
 SPLOSNA = {'S': 0} #splosna olajsava
 
-OSEBNA = {'O' : 1471.57} #osebna olajsava
+OSEBNA = {'O' : 1471.57} #osebna olajsava invalid
 
-POSEBNA_OSEBNA = {'P' : 3500} #posebna osebna olajsava na letni zasluzek
+POSEBNA_OSEBNA = {'P' : 3500} #posebna osebna olajsava na letni zasluzek, student
+
 
 POSEBNA_OTROSKA = {'Po' : 0}  #posebna olajsava za vzdrzevanega otroka
 na_otroka = {
@@ -15,12 +16,21 @@ na_otroka = {
         4:6187.85,
         5:7957.14
         }
+
+na_otroka_mesecno = {
+        0 : 0,
+        1 : 203.08,
+        2 : 220.77,
+        3 : 368.21,
+        4 : 515.65,
+        5 : 663.09
+        }
 POSEBNA_OTROSKA_INV = {'PoI' : 735.83} #posenbna olajsava za vzdrzevanega otroka, ki potrebuje posebno nego
 
 POSEBNA_CLAN = {'Pc' : 203.08} # posebna olajsava za vdrzevanega druziskega clana
 
 DODATNO_POKOJNINSKO = {'Z' : 0} #olajsava za prostovoljno dodatno pokojninsko zavarovanje
 
-
+UPOKOJENEC = 'Up'
 
 
