143ae22bd1a4d66d9f0ae87ee2e7bbf8c39500cf
Vito Rozman <rozman.vito@gmali.com>
2020-08-17 20:18:21 +0200

    dedal funkcijo za zacetno sran ki jo je treba se izpopolniti

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -4,7 +4,18 @@ import bottle
 
 @bottle.get('/')
 def zacetna_stran():
-    return 'Zivjo'
+    return bottle.template('zacetna_stran.html')
+
+@bottle.post('/izracunaj/')
+def izracunaj():
+    dohodek = int(bottle.request.forms['dohodek'])
+    prispevki = int(bottle.request.forms['prispevki'])
+    akontacija = int(bottle.request.forms['akontacija'])
+    bottle.redirect('/')
+    print(dohodek)
+    print(prispevki)
+    print(akontacija)
+
 
 
 
